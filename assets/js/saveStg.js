5115bf59f3c1e44280d4a9c5111b391887dd28e4
Tristan Germer <tgermer@hm.edu>
2020-07-15 17:35:04 +0200

    Cookies für das Speichern von STG

--- assets/js/saveStg.js
+++ assets/js/saveStg.js
@@ -2,9 +2,11 @@ $('.hideSaveStg').click(function() {
   // $('.toggleSave').toggleSave('none');
   $('.hideSaveStg').hide();
   $('.showSaveStg').show();
+  // document.cookie = stg=WW-M;expires=Wed, 18 Dec 2050 12:00:00 GMT;
 });
 $('.showSaveStg').click(function() {
   // $('.toggleSave').toggleSave('inline');
   $('.hideSaveStg').show();
   $('.showSaveStg').hide();
+  // document.cookie = stg=WW-M;expires=Wed, 18 Dec 2000 12:00:00 GMT;
 });
