16a4f4d4eca35e399b9cedd6091e9bdfeeab3939
Tristan Germer <tgermer@me.com>
2020-07-21 19:28:18 +0200

    Anderung zu Coockies

--- assets/js/saveStg.js
+++ assets/js/saveStg.js
@@ -2,11 +2,24 @@ $('.hideSaveStg').click(function() {
   // $('.toggleSave').toggleSave('none');
   $('.hideSaveStg').hide();
   $('.showSaveStg').show();
-  // document.cookie = stg=WW-M;expires=Wed, 18 Dec 2050 12:00:00 GMT;
+  console.log("Cookie STG entfernt");
 });
 $('.showSaveStg').click(function() {
   // $('.toggleSave').toggleSave('inline');
   $('.hideSaveStg').show();
   $('.showSaveStg').hide();
-  // document.cookie = stg=WW-M;expires=Wed, 18 Dec 2000 12:00:00 GMT;
+  console.log("Cookie STG hinzugefügt");
+});
+
+$('.merken').click(function() {
+  // $('.toggleSave').toggleSave('none');
+  $('.merken').hide();
+  $('.loeschen').show();
+  console.log("Cookie STG entfernt");
+});
+$('.loeschen').click(function() {
+  // $('.toggleSave').toggleSave('inline');
+  $('.merken').show();
+  $('.loeschen').hide();
+  console.log("Cookie STG hinzugefügt");
 });
