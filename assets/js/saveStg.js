2402fb83f585bfd48a967681cc0d8694c3407d86
Tristan Germer <tgermer@hm.edu>
2020-08-04 15:33:32 +0200

    Taste STG merken angepasst und toggelt jetzt.

--- assets/js/saveStg.js
+++ assets/js/saveStg.js
@@ -11,15 +11,15 @@ $('.showSaveStg').click(function() {
   console.log("Cookie STG hinzugefügt");
 });
 
-$('.merken').click(function() {
-  // $('.toggleSave').toggleSave('none');
-  $('.merken').hide();
-  $('.loeschen').show();
-  console.log("Cookie STG entfernt");
-});
-$('.loeschen').click(function() {
-  // $('.toggleSave').toggleSave('inline');
-  $('.merken').show();
-  $('.loeschen').hide();
-  console.log("Cookie STG hinzugefügt");
+// $('.merken').click(function() {
+//   // $('.toggleSave').toggleSave('none');
+//   $('.merken').hide();
+//   $('.loeschen').show();
+//   console.log("Cookie STG entfernt");
+// });
+// $('.loeschen').click(function() {
+//   // $('.toggleSave').toggleSave('inline');
+//   $('.merken').show();
+//   $('.loeschen').hide();
+//   console.log("Cookie STG hinzugefügt");
 });
